929763670bfdadbeba431432efa518bf505139cf
Bhavin Sheth <39247694+shethb@users.noreply.github.com>
2019-02-24 09:39:38 -0500

    Update movie_collection_project.py

--- movie_collection_project.py
+++ movie_collection_project.py
@@ -1,41 +1,40 @@
-movies = []
-matches = []
-movies.append({'Name': 'sherlock', 'Year': '2009', 'Genre': 'thriller'})
-movies.append({'Name': 'titanic', 'Year' : '2009', 'Genre': 'drama'})
-movies.append({'Name': 'Andhadhun', 'Year' : '2018', 'Genre': 'crime'})
-
-def find_movie():
-    choice = input("Please type 'name' to find by name/ 'year' to find by year/ 'genre' to find by genre\nHow do you want to find your movie:")
+class movies:
+    
+    def __init__(self):
+        self.movies = [{'name': 'sherlock', 'year': '2009', 'genre': 'thriller'},
+                       {'name': 'titanic', 'year': '2009', 'genre': 'drama'},
+                       {'name': 'andhadhun', 'year': '2018', 'genre': 'crime'}]
+    
+    def find_movie(self):
+        choice = input("Please type 'name' to find by name/ 'year' to find by year/ 'genre' to find by genre\nHow do you want to find your movie:")
         cs = input("\nWhat are you looking for?  ")
-        m = list(filter(lambda movie: movie[choice] == cs, movies))
+        m = list(filter(lambda movie: movie[choice] == cs, self.movies))
         for mo in m:
             print(f"\nMovie Name : {mo['name']}", f"  Movie Year : {mo['year']}", f"  Movie Genre : {mo['genre']}")
 
-def add_movie():
-    movies.append({'Name': input("Please input name:"), 'Year': input("Please input year:"), 'Genre': input("Please input genre:")})
-    print("\nThe movie you said is been added to your collection: \n", movies)
-    return movies
+    def add_movie(self):
+        self.movies.append({'name': input("Please input name:"), 'year': input("Please input year:"), 'genre': input("Please input genre:")})
+        print(f"\nThe movie you said is been added to your collection: \n", self.movies)
 
-def view_movie():
-    for movie in movies:
-        print(f"Movie Name : {movie['Name']}", f"  Movie Year : {movie['Year']}", f"  Movie Genre : {movie['Genre']}")
-    return movie
+    def view_movie(self):
+        for movie in self.movies:
+            print(f"Movie Name : {movie['name']}", f"  Movie Year : {movie['year']}", f"  Movie Genre : {movie['genre']}")
 
 
-running = True
+new_movie = movies()
+running  = True
 while running:
-    inp = input(
-        "Please type 'find' to find any movie/ 'add' to add new movie/ 'view' to view your collection/ 'end' to end program\nWhat would you like to do: ")
+    inp = input("Please type 'find' to find any movie/ 'add' to add new movie/ 'view' to view your collection/ 'end' to end program\nWhat would you like to do: ")
     if inp == "find":
-        find_movie()
+        new_movie.find_movie()
     elif inp == "add":
-        add_movie()
+        new_movie.add_movie()
     elif inp == "view":
-        view_movie()
+        new_movie.view_movie()
     elif inp == "end":
         print("\nThank you!")
         running = False
     else:
-        print("\nThank you!")
-        running = False
+        print("\nPlease try again\nThank you!")
+        running  = False
 
