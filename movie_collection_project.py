273aa744136638b57315c66667d1c30d47649076
Bhavin Sheth <39247694+shethb@users.noreply.github.com>
2019-03-01 15:09:33 -0500

    Update movie_collection_project.py

--- movie_collection_project.py
+++ movie_collection_project.py
@@ -6,9 +6,17 @@ class movies:
                        {'name': 'andhadhun', 'year': '2018', 'genre': 'crime'}]
     
     def find_movie(self):
-        choice = input("Please type 'name' to find by name/ 'year' to find by year/ 'genre' to find by genre\nHow do you want to find your movie:")
-        cs = input("\nWhat are you looking for?  ")
-        m = list(filter(lambda movie: movie[choice] == cs, self.movies))
+        try:
+            choice = input("Please type 'name' to find by name/ 'year' to find by year/ 'genre' to find by genre\nHow do you want to find your movie:")
+#             Here if choice is not equal to name/year/genre then it will not move further.
+            if choice=='name' or choice =='year' or choice=='genre':
+                cs = input("\nWhat are you looking for?  ")
+            m = list(filter(lambda movie: movie[choice] == cs, self.movies))
+        except:
+            print("Invalid option, Please try again with valid input.")
+            choice = input("Please type 'name' to find by name/ 'year' to find by year/ 'genre' to find by genre\nHow do you want to find your movie:")
+            cs = input("\nWhat are you looking for?  ")
+            m = list(filter(lambda movie: movie[choice] == cs, self.movies))
         for mo in m:
             print(f"\nMovie Name : {mo['name']}", f"  Movie Year : {mo['year']}", f"  Movie Genre : {mo['genre']}")
 
